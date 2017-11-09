1aa6affc1b6cb7578220a559967e0b4b5c733a97
Vitor Cavalcanti <vitorluizc@outlook.com>
2017-11-09 10:52:39 -0200

    Initialize as module + Add build & test scripts

---
+++ dist/vuex-handler.common.js
@@ -0,0 +1,40 @@
+'use strict';
+
+/**
+ * Install this plugin on Vuex.Store.
+ * @example ```js
+ * new Vuex.Store({ plugins: [ handler ], ... })
+ * ```
+ * @param {Vuex.Store} store 
+ */
+var install = function (store) {
+  var original = store.dispatch;
+
+  store.handler = {};
+
+  store.dispatch = function (type) {
+    var params = [], len = arguments.length - 1;
+    while ( len-- > 0 ) params[ len ] = arguments[ len + 1 ];
+
+    var action = original.apply(store, [ type ].concat( params ));
+    var ref = store.handler[type] || store.handler;
+    var onFailure = ref.onFailure;
+    var onSuccess = ref.onSuccess;
+
+    action
+      .then(function (result) { return Promise.resolve(
+        onSuccess
+          ? onSuccess.apply(void 0, [ result, type ].concat( params ))
+          : result
+      ); })
+      .catch(function (error) { return Promise.resolve(
+        onFailure
+          ? onFailure.apply(void 0, [ error, type ].concat( params ))
+          : Promise.reject(error)
+      ); });
+
+    return action
+  };
+};
+
+module.exports = install;
---
+++ dist/vuex-handler.js
@@ -0,0 +1,46 @@
+(function (global, factory) {
+	typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+	typeof define === 'function' && define.amd ? define(factory) :
+	(global.vuexHandler = factory());
+}(this, (function () { 'use strict';
+
+/**
+ * Install this plugin on Vuex.Store.
+ * @example ```js
+ * new Vuex.Store({ plugins: [ handler ], ... })
+ * ```
+ * @param {Vuex.Store} store 
+ */
+var install = function (store) {
+  var original = store.dispatch;
+
+  store.handler = {};
+
+  store.dispatch = function (type) {
+    var params = [], len = arguments.length - 1;
+    while ( len-- > 0 ) params[ len ] = arguments[ len + 1 ];
+
+    var action = original.apply(store, [ type ].concat( params ));
+    var ref = store.handler[type] || store.handler;
+    var onFailure = ref.onFailure;
+    var onSuccess = ref.onSuccess;
+
+    action
+      .then(function (result) { return Promise.resolve(
+        onSuccess
+          ? onSuccess.apply(void 0, [ result, type ].concat( params ))
+          : result
+      ); })
+      .catch(function (error) { return Promise.resolve(
+        onFailure
+          ? onFailure.apply(void 0, [ error, type ].concat( params ))
+          : Promise.reject(error)
+      ); });
+
+    return action
+  };
+};
+
+return install;
+
+})));
